5e93dc39196c26112598974b0bae86f664c644c8
steph bug <plexikongit@gmail.com>
2021-06-02 04:46:18 +0200

    do not start transaction if message will be dispatched async

--- src/Tracking/Subscribers/TransactionalHandlerSubscriber.php
+++ src/Tracking/Subscribers/TransactionalHandlerSubscriber.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Chronhub\Chronicler\Tracking\Subscribers;
 
 use Chronhub\Chronicler\Support\Contracts\Chronicler;
-use Chronhub\Foundation\Support\Contracts\Reporter\Reporter;
-use Chronhub\Foundation\Support\Contracts\Tracker\MessageTracker;
 use Chronhub\Chronicler\Support\Contracts\TransactionalChronicler;
+use Chronhub\Foundation\Message\Message;
+use Chronhub\Foundation\Support\Contracts\Message\Header;
+use Chronhub\Foundation\Support\Contracts\Reporter\Reporter;
 use Chronhub\Foundation\Support\Contracts\Tracker\ContextualMessage;
 use Chronhub\Foundation\Support\Contracts\Tracker\MessageSubscriber;
+use Chronhub\Foundation\Support\Contracts\Tracker\MessageTracker;
 
 final class TransactionalHandlerSubscriber implements MessageSubscriber
 {
@@ -19,18 +21,28 @@ final class TransactionalHandlerSubscriber implements MessageSubscriber
 
     public function attachToTracker(MessageTracker $tracker): void
     {
-        $tracker->listen(Reporter::DISPATCH_EVENT, function (): void {
-            if ($this->chronicler instanceof TransactionalChronicler) {
+        $tracker->listen(Reporter::DISPATCH_EVENT, function (ContextualMessage $context): void {
+            if ($this->isTransactionNeeded($context->message())
+                && $this->chronicler instanceof TransactionalChronicler) {
                 $this->chronicler->beginTransaction();
             }
         }, Reporter::PRIORITY_INVOKE_HANDLER + 1000);
 
         $tracker->listen(Reporter::FINALIZE_EVENT, function (ContextualMessage $context): void {
-            if ($this->chronicler instanceof TransactionalChronicler && $this->chronicler->inTransaction()) {
+            if ($this->isTransactionNeeded($context->message())
+                && $this->chronicler instanceof TransactionalChronicler
+                && $this->chronicler->inTransaction()) {
                 $context->hasException()
                     ? $this->chronicler->rollbackTransaction()
                     : $this->chronicler->commitTransaction();
             }
         }, 1000);
+    }
+
+    private function isTransactionNeeded(Message $message): bool
+    {
+        $isAsync = $message->header(Header::ASYNC_MARKER);
+
+        return null === $isAsync || true === $isAsync;
     }
 }
